81f75973502a98cc971bf276a72395dd041d52a5
sumit <kumarsumitsingh@gmail.com>
2022-11-18 17:08:24 -0500

    updating review comments

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,14 +1,16 @@
 import sqlite3
 import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 db_connection_count=0
-post_count=0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
+    db_connection_count=db_connection_count+1
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
@@ -17,7 +19,6 @@ def get_db_connection():
 def get_post(post_id):
     global db_connection_count
     connection = get_db_connection()
-    db_connection_count+=1
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
@@ -30,11 +31,10 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application 
 @app.route('/')
 def index():
-    global db_connection_count
     connection = get_db_connection()
-    db_connection_count+=1
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    logging.debug('Index page is retrieved')
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -43,16 +43,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      logging.info('404 is encountered')
+      logging.debug('404 is encountered')
       return render_template('404.html'), 404
     else:
-      logging.info("%s is fetched",post['title'] )
+      logging.debug("%s is fetched",post['title'] )
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
-    logging.info('About section is retrieved')
+    logging.debug('About section is retrieved')
     return render_template('about.html')
 
 
@@ -67,7 +67,11 @@ def healthcheck():
 
 @app.route('/metrics')
 def metrics():
-    logging.info('This is a call to metric api')
+    logging.debug('This is a call to metric api')
+    connection = get_db_connection()
+    cursor_obj = connection.cursor()
+    cursor_obj.execute('SELECT * FROM posts')
+    post_count=len(cursor_obj.fetchall())
     response = app.response_class(
             response=json.dumps({"db_connection_count":db_connection_count,"post_count":post_count}),
             status=200,
@@ -79,8 +83,6 @@ def metrics():
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
-    global db_connection_count
-    global post_count
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
@@ -89,8 +91,6 @@ def create():
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            db_connection_count=db_connection_count+1
-            post_count=post_count+1
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
             connection.commit()
@@ -102,6 +102,22 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    logging.info('This is a info')
+    logger = logging.getLogger('techtrends')
+    logger.setLevel(logging.DEBUG) 
+    
+    filehandler = logging.FileHandler('app.log', mode='w')
+    filehandler.setLevel(logging.DEBUG)
+    
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+
+    stderr_handler=logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.ERROR)
+
+    handlers = [stderr_handler, stdout_handler,filehandler]
+    
+    #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    logging.basicConfig(format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s", level=logging.DEBUG, handlers=handlers)
+    
     app.run(host='0.0.0.0', port='3111',debug=True)
